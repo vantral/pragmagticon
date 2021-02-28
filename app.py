5e4ba79eef2d5bee72913bc3bf0029e4aaf6fed9
Vantral <anton.buzanov.00@gmail.com>
2021-02-28 15:22:09 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,77 @@
+from typing import List
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import any_, or_
+from sqlalchemy.orm import sessionmaker
+from models import *
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+engine = create_engine('postgres+psycopg2://wnishjci:JtKBFR-ksN92wn6JS1PX9ZdG1Y3pj9DN@ziggy.db.elephantsql.com:5432/wnishjci')
+db.Model.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def merge_dfs(dfs: List["DF"]):
+    merged = {}
+    for df in dfs:
+        if df.df in merged:
+            check = merged[df.df]
+            if df.language == check['language'] and df.primary_semantics_id == check['primary_semantics_id'] \
+                    and df.speech_act_1_id == check['speech_act_1_id'] and df.speech_act_id == check['speech_act_id']:
+                check['additional_semantics'].append(df.additional_semantics.semantics)
+        else:
+            merged[df.df] = df.__dict__
+            merged[df.df]['additional_semantics'] = [df.additional_semantics.semantics]
+            merged[df.df]['primary_semantics'] = df.primary_semantics
+            merged[df.df]['speech_act'] = df.speech_act
+    return list(merged.values())
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['get'])
+def result():
+    if not request.args:
+        return redirect(url_for('hello'))
+    all_languages = any_(['ru', 'sr'])
+    all_semantics = any_([x.semantics for x in session.query(Semantics).all()])
+    all_speech_acts = any_([x.speech_act for x in session.query(SpeechAct).all()])
+    word = request.args.get('word')
+    language = request.args.get('language')
+    if not language:
+        language = all_languages
+    pr_sem = request.args.get('pr_sem')
+    if not pr_sem:
+        pr_sem = all_semantics
+    speech_act = request.args.get('speech_act')
+    if not speech_act:
+        speech_act = all_speech_acts
+    add_sem = request.args.getlist('add_sem')
+    if not add_sem:
+        add_sem = all_semantics
+    else:
+        add_sem = any_(add_sem)
+
+
+    new_query = session.query(DF)
+    records = new_query.filter(DF.language == language,
+                               DF.primary_semantics.has(Semantics.semantics == pr_sem),
+                               DF.speech_act.has(SpeechAct.speech_act == speech_act),
+                               DF.additional_semantics.has(Semantics.semantics == add_sem)).all()
+
+    records = merge_dfs(records)
+
+    return render_template('result.html', records=records)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,86 @@
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+
+from app import db
+
+engine = create_engine('postgresql+psycopg2://postgres:admin@localhost/postgres')
+
+# Base = declarative_base()
+
+
+class Intonation(db.Model):
+    __tablename__ = 'intonations'
+
+    id = Column(Integer, primary_key=True)
+    intonation = Column(String)
+
+
+class Semantics(db.Model):
+    __tablename__ = 'semantics'
+
+    id = Column(Integer, primary_key=True)
+    semantics = Column(String)
+
+
+class SpeechAct(db.Model):
+    __tablename__ = 'speech_acts'
+
+    id = Column(Integer, primary_key=True)
+    speech_act = Column(String)
+
+
+class DF(db.Model):
+    __tablename__ = 'dfs'
+
+    id = Column(Integer, primary_key=True)
+    df = Column(String)
+    language = Column(String)
+    glosses = Column(String)
+    syntax = Column(String)
+    primary_semantics_id = Column(Integer, ForeignKey('semantics.id'))
+    primary_semantics = relationship("Semantics", foreign_keys=[primary_semantics_id])
+    additional_semantics_id = Column(Integer, ForeignKey('semantics.id'))
+    additional_semantics = relationship("Semantics", foreign_keys=[additional_semantics_id])
+    speech_act_1_id = Column(Integer, ForeignKey("speech_acts.id"))
+    speech_act_1 = relationship("SpeechAct", foreign_keys=[speech_act_1_id])
+    speech_act_id = Column(Integer, ForeignKey("speech_acts.id"))
+    speech_act = relationship("SpeechAct", foreign_keys=[speech_act_id])
+    structure = Column(String)
+    intonation_id = Column(Integer, ForeignKey("intonations.id"))
+    intonation = relationship("Intonation", foreign_keys=[intonation_id])
+    source_construction = Column(String)
+    source_construction_syntax = Column(String)
+    source_construction_intonation_id = Column(Integer, ForeignKey("intonations.id"))
+    source_construction_intonation = relationship("Intonation", foreign_keys=[source_construction_intonation_id])
+    examples = Column(String)
+    comments = Column(String)
+
+    def __eq__(self, other: "DF"):
+        # print([self.df, other.df],
+        #       [self.language, other.language],
+        #       [self.glosses, other.glosses],
+        #       [self.syntax, other.syntax],
+        #       [self.primary_semantics_id, other.primary_semantics_id],
+        #       [self.additional_semantics_id, other.additional_semantics_id],
+        #       [self.speech_act_id, other.speech_act_id],
+        #       [self.speech_act_1_id, other.speech_act_1_id],
+        #       [self.structure, other.structure],
+        #       [self.intonation_id, other.intonation_id],
+        #       [self.source_construction, other.source_construction],
+        #       [self.source_construction_syntax, other.source_construction_syntax],
+        #       [self.source_construction_intonation_id, other.source_construction_intonation_id],
+        #       [self.examples, other.examples],
+        #       [self.comments, other.comments])
+
+        return str(self.df) == str(other.df) and str(self.language) == str(other.language) \
+               and str(self.glosses) == str(other.glosses) and str(self.syntax) == str(other.syntax) \
+               and str(self.primary_semantics_id) == str(other.primary_semantics_id) \
+               and str(self.additional_semantics_id) == str(other.additional_semantics_id) \
+               and str(self.speech_act_id) == str(other.speech_act_id) and str(self.speech_act_1_id) == str(
+            other.speech_act_1_id) \
+               and str(self.structure) == str(other.structure) and str(self.intonation_id) == str(other.intonation_id) \
+               and str(self.source_construction) == str(other.source_construction) \
+               and str(self.source_construction_syntax) == str(other.source_construction_syntax) \
+               and str(self.source_construction_intonation_id) == str(other.source_construction_intonation_id) \
+               and str(self.examples) == str(other.examples) and str(self.comments) == str(other.comments)
